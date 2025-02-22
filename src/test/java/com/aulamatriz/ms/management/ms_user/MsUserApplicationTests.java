50301290be1f30e8136cbc550c558ee8c37a690a
djosuef <darwinjof@gmail.com>
2025-02-22 07:54:41 -0500

    Commit project

---
+++ src/test/java/com/aulamatriz/ms/management/ms_user/MsUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aulamatriz.ms.management.ms_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
